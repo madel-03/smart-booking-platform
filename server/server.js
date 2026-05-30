505969f4d06df8f36e7c3da529a2c49fd92c368f
madel-03 <m5726738@gmail.com>
2026-05-31 02:11:46 +0300

    fix cors and env variables

--- server/server.js
+++ server/server.js
@@ -1,18 +1,22 @@
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
+ 
 const app = express();
-
-app.use(cors());
+ 
+app.use(cors({
+  origin: 'https://smart-booking-platform-lilac.vercel.app'
+}));
 app.use(express.json());
-
-// رابط MongoDB الصحيح
-const MONGO_URI = 'mongodb://admin:admin10@ac-rnj5kbb-shard-00-00.5buf02i.mongodb.net:27017,ac-rnj5kbb-shard-00-01.5buf02i.mongodb.net:27017,ac-rnj5kbb-shard-00-02.5buf02i.mongodb.net:27017/?ssl=true&replicaSet=atlas-6cjcc7-shard-0&authSource=admin&appName=Cluster0';// الاتصال بقاعدة البيانات
+ 
+// رابط MongoDB من البيئة
+const MONGO_URI = process.env.MONGODB_URI;
+ 
 mongoose.connect(MONGO_URI)
     .then(() => console.log('✅ تم الاتصال بقاعدة البيانات بنجاح!'))
     .catch(err => console.error('❌ خطأ في الاتصال بقاعدة البيانات:', err));
-
+ 
 // إنشاء Schema
 const appointmentSchema = new mongoose.Schema({
     customerName: String,
@@ -25,10 +29,10 @@ const appointmentSchema = new mongoose.Schema({
         default: 'Pending'
     }
 }, { versionKey: false });
-
+ 
 // إنشاء Model
 const Appointment = mongoose.model('Appointment', appointmentSchema);
-
+ 
 // جلب جميع الحجوزات
 app.get('/api/appointments', async (req, res) => {
     try {
@@ -38,27 +42,25 @@ app.get('/api/appointments', async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 });
-
+ 
 // إضافة حجز جديد
 app.post('/api/appointments', async (req, res) => {
     try {
         const newAppointment = new Appointment(req.body);
-
         const savedAppointment = await newAppointment.save();
-
         res.status(201).json({
             message: '🎉 تم تسجيل الحجز بنجاح!',
             appointment: savedAppointment
         });
-
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
 });
-
+ 
 // تشغيل السيرفر
-const PORT = 5000;
-
+const PORT = process.env.PORT || 5000;
+ 
 app.listen(PORT, () => {
     console.log(`🔥 Server running on port ${PORT}`);
 });
+ 
